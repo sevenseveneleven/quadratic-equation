b226a761ef770c2d1d67dec0504c3a0afe06bc58
aaa <it-z@tut.by>
2018-03-01 06:13:17 +0300

    solved anyway

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
+	var eq = equation.replace(/\s/g, '');
+	var s1 = eq.split(['*x^2']);
+	var a = parseInt(s1[0]);
+	var s2 = s1[1].split(['*x']);
+	var b = parseInt(s2[0]);
+	var c = parseInt(s2[1]);
+	var root = Math.round((((-1*b) + Math.sqrt(Math.pow(b,2) - (4*a*c)))/(2*a)));
+	var root2 = Math.round((((-1*b) - Math.sqrt(Math.pow(b,2) - (4*a*c)))/(2*a)));
+	if (root2 > root) 
+		return [root, root2]
+	else
+		return [root2, root]
 }
